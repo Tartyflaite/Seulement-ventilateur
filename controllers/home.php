a580f4948fb1fb24d3e9c67fed15588fc4e88ff5
Syltenor <74373766+RaphBhz@users.noreply.github.com>
2021-10-23 18:43:49 +0200

    beginning of navbar and logos

--- controllers/home.php
+++ controllers/home.php
@@ -1,11 +1,3 @@
 <?php
 
-include(ROOT.'/views/home.html');
-if(isset($_SESSION['connected']) && $_SESSION['connected'] == true){
-include(ROOT.'/views/navbar/navbar.html');
-}
-else{
-include(ROOT.'/views/navbar/navbar_notconnected.html');
-}
-
-include(ROOT.'/views/endofpage.html');
+include(ROOT . '/views/home.html');
