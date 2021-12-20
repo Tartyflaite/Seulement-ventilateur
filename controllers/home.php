8b27b0121a0e9a0d759422839b9d49365db6df4c
elkidd <stefan.rvic@gmail.com>
2021-12-20 03:44:49 +0100

    2 3 ajout avant de dormir

--- controllers/home.php
+++ controllers/home.php
@@ -1,7 +1,10 @@
 <?php
 if(isset($_SESSION['connected']) and $_SESSION['connected'] == true){
     include(ROOT . '/views/home.html');
+    $_SESSION['flash']['error'] = null;
 }
 else{
+    $_SESSION['flash']['error'] = 'Identifiez vous pour accéder au contenu';
     include(ROOT . '/views/index.html');
+    $_SESSION['flash']['error'] = null;
 }
--- controllers/index.php
+++ controllers/index.php
@@ -1,6 +1,7 @@
 <?php
 if(isset($_SESSION['connected']) and $_SESSION['connected'] == true){
     include(ROOT . '/views/home.html');
+    $_SESSION['flash']['error'] = null;
 }
 else{
     include(ROOT . '/views/index.html');
